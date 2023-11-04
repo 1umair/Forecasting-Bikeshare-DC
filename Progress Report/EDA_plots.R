d9433e4f7170fc254025b87211ca1e852ab0035a
Josh Blakely <jblakely8@gatech.edu>
2023-11-03 23:50:46 -0500

    Clean up charts. Working on wording. Make tables cleaner.

--- Progress Report/EDA_plots.R
+++ Progress Report/EDA_plots.R
@@ -3,7 +3,7 @@ library(ggplot2)
 require(gridExtra)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-df <- read_csv("../Data/day.csv") |>
+day <- read_csv("../data/day.csv") |>
   rename(
     weather = weathersit,
     total_count = cnt
@@ -15,16 +15,15 @@ df <- read_csv("../Data/day.csv") |>
     workingday = as.factor(workingday),
     weather = as.factor(weather)
   )
-
-levels(df$season) <- c('winter', 'spring', 'summer', 'fall')
-levels(df$workingday) <- c('weekend or holiday', 'weekday')
-levels(df$weekday) <- c('Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat')
-levels(df$holiday) <- c('non-holiday', 'holiday')
-levels(df$weather) <- c('clear', 'misty', 'light rain', 'heavy rain')
+levels(day$season) <- c('winter', 'spring', 'summer', 'fall')
+levels(day$workingday) <- c('weekend or holiday', 'weekday')
+levels(day$weekday) <- c('Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat')
+levels(day$holiday) <- c('non-holiday', 'holiday')
+levels(day$weather) <- c('clear', 'misty', 'light rain', 'heavy rain')
 
 
 # Look at daily usage of bikes on a seasonality plot
-ggplot(data = df, mapping = aes(x = instant, y = total_count)) +
+ggplot(data = day, mapping = aes(x = instant, y = total_count)) +
   geom_point(aes(color = season)) +
   labs(
     x = "Index of bike usage",
@@ -34,7 +33,7 @@ ggplot(data = df, mapping = aes(x = instant, y = total_count)) +
   )
 
 # See bike usage on a daily for day of the week
-df |>
+day |>
   ggplot(mapping = aes(x = instant, y = total_count)) +
     geom_point(aes(color = weekday)) +
     labs(
@@ -45,7 +44,7 @@ df |>
     )
 
 # Does temperature and day of the week have an affect on the bike usage.
-df |>
+day |>
   ggplot(mapping = aes(x = instant, y = temp)) +
   geom_point(aes(color = weekday, size = total_count / 1000)) +
   scale_size_continuous() +
@@ -58,32 +57,32 @@ df |>
   )
 
 # How does the usage for all seasons look based on day of the week
-df |>
+day |>
   ggplot(mapping = aes(weekday, total_count, color = season)) +
   geom_boxplot()
 
 # How does the usage differ between holiday and non-holiday
-df |>
+day |>
   ggplot(mapping = aes(x = holiday, y = total_count)) +
   geom_boxplot()
 
 # What type of weather do people typically ride the bikes?
-df |>
+day |>
   ggplot(mapping = aes(x = weather, y = total_count)) +
   geom_boxplot()
 
 # Heat map of weather and weekday.
-df |>
+day |>
   ggplot(mapping = aes(x = weather, y = weekday, fill = total_count)) +
   geom_tile()
 
-df |>
+day |>
   ggplot(mapping = aes(x = season, y = weekday, fill = total_count)) +
   geom_tile() +
   scale_fill_distiller(palette = 'Spectral')
 
 
-fit <- lm(total_count ~ ., df)
+fit <- lm(total_count ~ ., day)
 summary(fit)
 
 # Hourly -------
